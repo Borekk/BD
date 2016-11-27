29d0925a35704a25e48cccd0476da23d3a18d61c
Borekk <karbor@student.agh.edu.pl>
2016-11-27 15:10:08 +0100

    Update kolos.sql

--- kolos.sql
+++ kolos.sql
@@ -16,3 +16,17 @@ WHERE p.idmeczu IN (
 					SELECT DISTINCT m.idmeczu
 					FROM punktujace p JOIN siatkarki s ON p.iddruzyny=s.iddruzyny AND p.numer=s.numer JOIN mecze m ON p.idmeczu=m.idmeczu JOIN druzyny d ON s.iddruzyny=d.iddruzyny
 WHERE date_part('month', m.termin)=10 AND date_part('year', m.termin)=2009 AND s.numer=4 AND d.miasto='Piła');
+
+
+
+--zadanie z update
+
+--do sprawdzenia rezultatu
+select s.numer, s.imie, s.nazwisko, p.punkty from siatkarki s join punktujace p using(numer, iddruzyny) natural join druzyny d
+where d.iddruzyny like 'pila' and s.numer = 4 and p.idmeczu = 21;
+
+update punktujace p set punkty = 212
+from siatkarki s, druzyny d
+where
+s.numer = p.numer and s.iddruzyny = p.iddruzyny and p.iddruzyny = d.iddruzyny and
+ d.iddruzyny like 'pila' and s.numer = 4 and idmeczu = 21;
